8eb95462bed08a25a0578fad12f89ea6c42c6f84
S.M. Hasan <s.m.hasan4599@gmail.com>
2026-04-24 16:39:24 +0600

    Fix: Explicitly cast API handler response to bypass TS error

--- frontend/src/app/api/[[...path]]/route.ts
+++ frontend/src/app/api/[[...path]]/route.ts
@@ -6,25 +6,25 @@ import serverless from 'serverless-http';
 const handler = serverless(app);
 
 export const GET = async (req: Request) => {
-  return handler(req, {});
+  return (handler(req, {}) as any);
 };
 
 export const POST = async (req: Request) => {
-  return handler(req, {});
+  return (handler(req, {}) as any);
 };
 
 export const PUT = async (req: Request) => {
-  return handler(req, {});
+  return (handler(req, {}) as any);
 };
 
 export const DELETE = async (req: Request) => {
-  return handler(req, {});
+  return (handler(req, {}) as any);
 };
 
 export const PATCH = async (req: Request) => {
-  return handler(req, {});
+  return (handler(req, {}) as any);
 };
 
 export const OPTIONS = async (req: Request) => {
-  return handler(req, {});
+  return (handler(req, {}) as any);
 };
