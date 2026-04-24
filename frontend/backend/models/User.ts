cf52d273851e9ba595425f80e727a4cfd854aaf6
S.M. Hasan <s.m.hasan4599@gmail.com>
2026-04-24 21:17:46 +0600

    Fix: Type Mongoose pre-save next callback in User model

--- frontend/backend/models/User.ts
+++ frontend/backend/models/User.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Document, Schema, CallbackWithoutResultAndOptionalError } from "mongoose";
 import bcrypt from "bcryptjs";
 
 
@@ -41,7 +41,7 @@ const userSchema = new Schema<IUser>({
 
 
 //comparing passwords
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function (next: CallbackWithoutResultAndOptionalError) {
     if(!this.isModified('password')) return next(); //if password is not modified then return
     const salt = await bcrypt.genSalt(10); //if password is modified then hash it
     this.password = await bcrypt.hash(this.password!, salt);
