bab73a5a2af48781cf8f18d5827b545b818017d0
S.M.Hasan <s.m.hasan4599@gmail.com>
2026-04-26 19:46:24 +0600

    Update multer temporary storage path to '/tmp/'

--- backend/config/cloudinaryConfig.ts
+++ backend/config/cloudinaryConfig.ts
@@ -5,15 +5,19 @@ import { RequestHandler } from "express";
  
 dotenv.config(); // Load environment variables from .env file
 
+
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME as string,
     api_key: process.env.CLOUDINARY_API_KEY as string,
     api_secret: process.env.CLOUDINARY_API_SECRET as string,
 });
 
+
 interface CustomFile extends Express.Multer.File {
     path: string;
 }
+
+
 
 
 // Multer configuration for file upload
@@ -21,6 +25,7 @@ const uploadToCloudinary = (file: CustomFile): Promise<UploadApiResponse> => {
     const options: UploadApiOptions = {
         resource_type: "image", // or "raw" for non-image files
     };
+
 
     return new Promise((resolve, reject) => {
         cloudinary.uploader.upload(file.path, options, (error, result) => {
@@ -33,8 +38,12 @@ const uploadToCloudinary = (file: CustomFile): Promise<UploadApiResponse> => {
 }
 
 
+
+
 //use multer to store images locally, temporarily and then upload to cloudinary
-const multerMiddleware: RequestHandler = multer({ dest: 'uploads/' }).array('images',4); //dest is the folder name where images are stored temporarily and 4 images can be uploaded at a time
+const multerMiddleware: RequestHandler = multer({ dest: '/tmp/' }).array('images',4); //dest is the folder name where images are stored temporarily and 4 images can be uploaded at a time
+
+
 
 
 export { uploadToCloudinary, multerMiddleware };
