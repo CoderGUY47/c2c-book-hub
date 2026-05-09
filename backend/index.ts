2e35545ce78a6113dca4430dac7324b70ea68fcb
S.M. Hasan <s.m.hasan4599@gmail.com>
2026-05-10 00:56:44 +0600

    fix payload limit for base64 image upload

--- backend/index.ts
+++ backend/index.ts
@@ -27,9 +27,9 @@ const corsOption = {
 };
 
 app.use(cors(corsOption));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
+app.use(express.json({ limit: '5mb' }));
+app.use(bodyParser.json({ limit: '5mb' }));
 app.use(passport.initialize());
 app.use(cookiesParser());
 
